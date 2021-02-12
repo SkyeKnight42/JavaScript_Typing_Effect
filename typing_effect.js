d5bd2f099b82f0487082567bd9060fa2e4b1feca
Skye Breeze <skyexwarren@gmail.com>
2021-02-12 16:55:56 +0000

    Fix

--- typing_effect.js
+++ typing_effect.js
@@ -1,57 +1,143 @@
 
+let typing = document.getElementById("typing")
+let dash = document.getElementById("dash")
+let blink = document.getElementById("blink")
 
+let typingElements = []
+let classNamesInUse = []
 // Begin the code when the page is loaded.
 document.addEventListener("DOMContentLoaded", function() {
 
-    TypeOutText(slowTypingElements[0], 500, "blink")
+    // TypeOutText(typing, 50, "typing", null)
+    // TypeOutText(dash, 100, "dash", "|")
+    // TypeOutText(blink, 150, "blink", "|")
+    // console.log(typing.classList)
 })
 
-// Element, the speed (milliseconds), normal typing ("typing"), dash ("dash"), or blinking ("blink")
-function TypeOutText(element, speed, style) {
-    let text = element.textContent
-    element.textContent = ""
+// the HTML element, the speed (milliseconds), normal typing ("typing"), dash ("dash"), or blinking ("blink"), and the character for the blink/dash effect as a string.
+function TypeOutText(element, speed, style, endcharacter) {
+    if (FindInArray(element, typingElements) == null) {
 
-    switch (style) {
-        case "typing":
+        let text = element.textContent
+        let customClass = CreateCustomClassName()
+        
+        if (customClass != null) {
 
-            for (let x = 0; x < text.length; x++) {
-                setTimeout(function() {
-                    element.textContent += text.charAt(x)
-                }, x * speed)
-            }
-
-            break;
-        case "dash":
-
-            for (let x = 0; x < text.length; x++) {
-                setTimeout(function() {
-                    let updatedText = element.textContent
-                    text = updatedText.slice(0, updatedText.length - 2)
-
-                }, x * speed)
-                setTimeout(function() {
-                    element.textContent += text.charAt(x) + " |"
-                }, x * speed)
-            }
-
-            break;
-        case "blink":
-
-            for (let x = 0; x < text.length; x++) {
-                setTimeout(function() {
-                    if (x % 2 == 0) {
-                        element.textContent += text.charAt(x) + "|"
-                    } else {
-                        element.textContent = element.textContent.slice(0, element.textContent.length - 1)
-                        element.textContent += text.charAt(x)
+            classNamesInUse.push(customClass)
+            element.classList.add(customClass)
+            typingElements.push(element)
+            element.textContent = ""
+            
+            switch (style) {
+                case "typing":
+        
+                    for (let x = 0; x <= text.length; x++) {
+                        setTimeout(function() {
+                            element.textContent += text.charAt(x)
+                            if (x == text.length) {
+                                element.classList.remove(customClass)
+                                RemoveArrayElement(customClass, classNamesInUse)
+                                RemoveArrayElement(element, typingElements)
+                            }
+                        }, x * speed)
                     }
 
-                }, x * speed)
+                    break;
+
+                case "dash":
+                    
+                    for (let x = 0; x <= text.length; x++) {
+                        setTimeout(function() {
+                            element.textContent = element.textContent.slice(0, element.textContent.length - endcharacter.length)
+                            element.textContent += text.charAt(x) + endcharacter
+                            if (x == text.length) {
+                                element.classList.remove(customClass)
+                                RemoveArrayElement(customClass, classNamesInUse)
+                                RemoveArrayElement(element, typingElements)
+
+                                if (text.length % 2 == 0) {
+                                    element.textContent = text
+                                }
+                            }
+
+                        }, x * speed)
+                    }
+        
+                    break;
+
+                case "blink":
+        
+                    for (let x = 0; x <= text.length; x++) {
+                        setTimeout(function() {
+                            if (x % 2 == 0) {
+                                element.textContent += text.charAt(x) + endcharacter
+                            } else {
+                                element.textContent = element.textContent.slice(0, element.textContent.length - endcharacter.length)
+                                element.textContent += text.charAt(x)
+                            }
+
+                            if (x == text.length) {
+                                element.classList.remove(customClass)
+                                RemoveArrayElement(customClass, classNamesInUse)
+                                RemoveArrayElement(element, typingElements)
+
+                                if (text.length % 2 == 0) {
+                                    element.textContent = text
+                                }
+                            }
+        
+                        }, x * speed)
+                    }
+        
+                    break;
+                
+                default:
+        
+                    break;
             }
+        } else {
 
-            break;
-        default:
+        }
+    } else {
 
-            break;
     }
+}
+
+function FindInArray(value, array) {
+    for (let x = 0; x < array.length; x++) {
+        if (value === array[x]) {
+            return x
+        }
+    }
+    return null
+}
+
+function RemoveArrayElement(value, array) {
+
+    for (let x = 0; x < array.length; x++) {
+
+        // console.log("value: " + value)
+        // console.log("arrayValue: " + array[x])
+
+        if (array[x] === value) {
+            array.splice(x, 1)
+            x--
+            // console.log(array[x] + "element is found and removed from the array.")
+        } else {
+            // console.log(array[x] + "element is not the typed element, keep it in the array.")
+        }
+    }
+}
+
+function CreateCustomClassName() {
+    
+    for (let x = 0; x < 100; x++) {
+        let customClassName = "TypingElement_" + x
+
+        if (classNamesInUse[x] != customClassName) {
+            return customClassName
+        }
+    }
+
+    return null
 }
